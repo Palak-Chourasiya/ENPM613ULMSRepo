3a3bdf098880af014c9dcbb221d1e55c9ec535f1
IJ <ijaginam@umd.edu>
2018-11-28 11:42:58 -0500

    IJ

--- src/main/java/ulms/courses/modules/CourseModuleController.java
+++ src/main/java/ulms/courses/modules/CourseModuleController.java
@@ -1,29 +1,16 @@
 package ulms.courses.modules;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import ulms.login.models.AccountEntity;
-import ulms.messages.messageEntity;
-import ulms.students.StudentEnrollmentService;
 
 @Controller
 @RequestMapping(path="/module")
