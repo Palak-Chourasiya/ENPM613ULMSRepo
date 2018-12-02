88b7944719fb236b7496824e6a5d7a9e9f52a866
Justin Miller <justinmiller.20732@gmail.com>
2018-12-02 15:54:02 -0500

    Code cleanup

--- src/main/java/ulms/courses/SyllabusController.java
+++ src/main/java/ulms/courses/SyllabusController.java
@@ -10,22 +10,9 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import ulms.login.models.AccountEntity;
-import ulms.messages.messageEntity;
-import ulms.students.StudentEnrollmentService;
 
 @Controller
 @RequestMapping(path="/syllabus")
