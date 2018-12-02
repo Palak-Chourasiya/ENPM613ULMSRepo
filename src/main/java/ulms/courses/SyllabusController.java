143408a5d9bb6e1baa62a3d0972704fd78f9d5cf
radzo1 <radzo1@morgan.edu>
2018-12-02 11:39:32 -0500

    raoul_syllabus_cousremodule

---
+++ src/main/java/ulms/courses/SyllabusController.java
@@ -0,0 +1,38 @@
+package ulms.courses;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ulms.login.models.AccountEntity;
+import ulms.messages.messageEntity;
+import ulms.students.StudentEnrollmentService;
+
+@Controller
+@RequestMapping(path="/syllabus")
+public class SyllabusController {
+
+	@GetMapping("/")
+    public String ksldjf(HttpServletRequest request) {
+		return "syllabusAmericanCuisine";
+    }
+}
---
+++ src/main/webapp/components/course/coursecomponent.js
@@ -0,0 +1,34 @@
+angular.module('ULMS')
+  .component('course', {
+    templateUrl: 'components/course/course.html',
+    bindings: {},
+    
+    controller: function($scope, $http) {
+
+        var ctrl = this;
+        
+        this.$onInit = function() {
+          
+        };
+        
+        $scope.course = [];
+
+        //Now load the data from server
+        _refreshCourseData();
+
+            var method = "";
+            var url = "";
+         
+        //HTTP GET- get all courses collection
+        function _refreshCourseData() {
+            $http({
+                method : 'GET',
+                url : 'http://localhost:8080/courses/1'
+            }).then(function successCallback(response) {
+                $scope.course = response.data;
+            }, function errorCallback(response) {
+                console.log(response.statusText);
+            });
+        }
+      }
+    })
